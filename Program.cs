b38c76b22873d97523a9ba65b75f8d97755cac78
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2023-12-10 12:59:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecsediTamas_beadando1210
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
